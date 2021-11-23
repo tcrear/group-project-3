578f051330930747ebf260c7054e0286402e776b
Cygnet717 <kathybradt717@gmail.com>
2021-11-23 13:03:14 -0600

    login and create user working

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require('./routes');
 
-var app = express();
-var PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 app.use(express.urlencoded({ extended: true }));
