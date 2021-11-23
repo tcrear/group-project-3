8ac7f021a43b5748700566baf3327602d340050b
Ben Camarillo <jtby97@gmail.com>
2021-11-23 14:06:07 -0600

    fixed goof with WishListCards

--- client/src/components/WishListCard.js
+++ client/src/components/WishListCard.js
@@ -1,29 +1,49 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 function WishList() {
   return (
-    <Card xs={1} md={2} className="g-4">
-      <CardMedia component="img" alt="game cover" height="140" image="" />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Game Title
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Information about the game will be displayed here
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+    <CardGroup>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Game Title</Card.Title>
+      <Card.Text>
+       This game is heckin cool!
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Release Date!</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Game Title</Card.Title>
+      <Card.Text>
+      This game is heckin cool!
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Release Date!</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Game Title</Card.Title>
+      <Card.Text>
+      This game is heckin cool!
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Release Date!</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
+
+  )
 }
 
 export default WishList;
