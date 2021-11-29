70a7991628a3ad933ea44c7050d8e929b47648ba
Preston <prestonconley5@gmail.com>
2021-11-29 14:08:48 -0600

    getting data to populate page

--- client/src/pages/SingleGame.js
+++ client/src/pages/SingleGame.js
@@ -10,6 +10,21 @@ function SingleGame(props) {
   const released = '2021-12-08'
   const esrb_rating = 'E'
 
+  let getDetails = function (rawgId) {
+    let apiUrl = `https://api.rawg.io/api/games?key=1cbc00cd5769401bbb4edd748b66b08c&dates=2019-09-01,2019-09-30&id=${rawgId}`;
+  
+    fetch(apiUrl)
+        .then(function(res){
+            if (res.ok){
+                res.json().then(function(data){
+                    console.log(data)
+                    setRawgDetails(res.json())
+                })
+            }
+        })
+  };
+  getDetails(rawgId)
+  
 
   return (
     <CardGroup>
