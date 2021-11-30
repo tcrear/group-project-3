ae6ec7286fc0b95a95780a20ad92c7163d40d9c8
Cygnet717 <kathybradt717@gmail.com>
2021-11-29 18:28:04 -0600

    trying to get rid of false error

--- client/src/components/WishListCard.js
+++ client/src/components/WishListCard.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import noImage from './images/No_image_available.svg'
 
 function WishListCard(props) {
-
   return (
     <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Img variant="top" src={noImage}/>
         <Card.Body>
           <Card.Title>
            {props.wish.title}
