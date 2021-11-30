42facc2e86accae27ce603ed5d88e54959b43f54
Ben Camarillo <jtby97@gmail.com>
2021-11-29 18:57:56 -0600

    MyGames.js functioning buttons

--- client/src/components/MyGamesCard.js
+++ client/src/components/MyGamesCard.js
@@ -1,26 +1,23 @@
 import React, {useState} from 'react';
 import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
 
 function GameCards(props) {
-  const [edit, setEdit] = useState({
-    title: '',
-    rating: ''
-  });
   return (
-    <CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      {/* needs to be props */}
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>
-       {props.rating}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-</CardGroup>
-
-  )
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>{props.gameItem.title}</Card.Title>
+      </Card.Body>
+      <Card.Footer>
+        {/* <button className="btn btn-sm" onClick={() => props.setGameToPlayed(props.wish)}>Mark This Game As Played</button> */}
+        <button
+          className="btn btn-sm"
+          onClick={() => props.removeGameListItem(props.gameItem.rawgId)}
+        >
+          Remove This Game
+        </button>
+      </Card.Footer>
+    </Card>
+  );
 }
 export default GameCards;
--- client/src/components/WishListCard.js
+++ client/src/components/WishListCard.js
@@ -2,23 +2,28 @@ import React, {useState, useEffect} from 'react';
 import Card from 'react-bootstrap/Card'
 
 function WishListCard(props) {
-
   return (
     <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>
-           {props.wish.title}
-          </Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <button className="btn btn-sm" onClick={() => props.setGameToPlayed(props.wish)}>Mark This Game As Played</button>
-          <button className="btn btn-sm" onClick={() => props.removeWishListItem(props.wish)}>Remove This Game</button>
-        </Card.Footer>
-
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>{props.wish.title}</Card.Title>
+      </Card.Body>
+      <Card.Footer>
+        <button
+          className="btn btn-sm"
+          onClick={() => props.setGameToPlayed(props.wish)}
+        >
+          Mark This Game As Played
+        </button>
+        <button
+          className="btn btn-sm"
+          onClick={() => props.removeWishListItem(props.wish)}
+        >
+          Remove This Game
+        </button>
+      </Card.Footer>
     </Card>
-    
-  )
+  );
 }
 
 export default WishListCard;
